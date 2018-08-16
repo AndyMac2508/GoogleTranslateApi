a9852c35e259795fdbe97768e06e0bb2857438dc
AndyMac2508 <andrew.mcdonald@rapidweb.biz>
2018-08-16 11:30:58 +0100

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 use AndyMac\GoogleTranslator\Objects\GoogleTranslator;
 use AndyMac\GoogleTranslator\Objects\JapanTranslate;
 
-$translator = new GoogleTranslator("AIzaSyDW9brX189036CgRXhXK-gfpE1WGqvmKWg");
+$translator = new GoogleTranslator("[API KEY]");
 
 $language = $translator->detectLanguage("Hallo hell how are you Schönen Tag");
 
